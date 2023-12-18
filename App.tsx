3d4ae4325425fe5341c4e43bbac0f3d2c0923f46
darde <pablodarde@gmail.com>
2023-12-18 18:49:17 -0300

    Add HK permissions

--- App.tsx
+++ App.tsx
@@ -2,13 +2,65 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StatsCard from './components/StatsCard';
 import RingProgress from './components/RingProgress';
+import AppleHealthKit, { HealthInputOptions, HealthKitPermissions } from 'react-native-health'
+import { useEffect, useState } from 'react';
+
+const permissiions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.StepCount,
+      AppleHealthKit.Constants.Permissions.DateOfBirth,
+      AppleHealthKit.Constants.Permissions.BiologicalSex,
+      AppleHealthKit.Constants.Permissions.Height,
+      AppleHealthKit.Constants.Permissions.Weight,
+    ],
+    write: [],
+  }
+}
+
+const STEPS_GOAL = 10000
 
 export default function App() {
+  const [hasPermissions, setHasPermissions] = useState(false)
+  const [steps, setSteps] = useState(0)
+
+  useEffect(() => {
+    AppleHealthKit.initHealthKit(permissiions, (err) => {
+      if (err) {
+        console.log('Error getting permissions. ', err)
+        return
+      }
+
+      setHasPermissions(true)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      return
+    }
+
+    const options: HealthInputOptions = {
+      date: new Date().toISOString(),
+      includeManuallyAdded: false,
+    }
+
+    AppleHealthKit.getStepCount(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the stpes. ', err)
+        return
+      }
+
+      setSteps(results.value)
+      console.log('steps results: ', results.value)
+    })
+  }, [hasPermissions])
+
   return (
     <View style={styles.container}>
-      <RingProgress progress={0.5} />
+      <RingProgress progress={steps / STEPS_GOAL} />
       <View style={styles.statsContainer}>
-        <StatsCard label='Steps' value={1219} />
+        <StatsCard label='Steps' value={steps.toString()} />
         <StatsCard label='Distance' value={`${0.75} Km`} />
         <StatsCard label='Flights Climbed' value={3} />
       </View>
